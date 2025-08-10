a2a938870ebb5f608cb7ff23e1f31eb465984ee8
Dzmitry Halavach <kotik1@protonmail.com>
2025-08-10 20:47:42 +0300

    refactor: nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+//import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -7,13 +7,13 @@ import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Star Wars Character Search Application',
-  description: 'Search and explore Star Wars characters',
-  icons: {
-    icon: '/icons8-star-wars.svg',
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'Star Wars Character Search Application',
+//   description: 'Search and explore Star Wars characters',
+//   icons: {
+//     icon: '/icons8-star-wars.svg',
+//   },
+// };
 
 export default function RootLayout({
   children,
